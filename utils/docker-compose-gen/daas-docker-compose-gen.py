412004289173b562b77d9afaa9f699ad1294e157
Pavel Vaynerman <pv@etersoft.ru>
2018-03-12 02:06:23 +0300

    (utils): добавил генерирование Dockerfile для builder

--- utils/docker-compose-gen/daas-docker-compose-gen.py
+++ utils/docker-compose-gen/daas-docker-compose-gen.py
@@ -114,15 +114,37 @@ def make_nodes(project, ctype, image):
 
 
 def get_image_name(project, image):
-    return "%s-%s" % (project['name'], image)
+    name = "%s-%s" % (project['name'], image)
+    if len(project['image-registry']) > 0:
+        name = "%s/%s" % (project['image-registry'], name)
+    if len(project['image-postfix']) > 0:
+        name = "%s%s" % (name, project['image-postfix'])
+
+    return name
+
+
+def make_dockerfile(dirname, tplname, project):
+    if not os.path.exists(dirname):
+        os.mkdir(dirname)
+
+    dockerfile = os.path.join(dirname, 'Dockerfile')
+    with open(dockerfile, 'w') as wfile:
+        wfile.write(env.get_template(tplname).render(project=project))
+        wfile.write('\n')  # fix bug: jinja cuts off the last line feed
 
 
 def usage():
-    print "%s [-c|--confile] project.yml [command]" % sys.argv[0]
+    print "%s [-c|--confile] project.yml [options] command" % sys.argv[0]
     print "Commands:"
     print "---------"
-    print "gen            - Generate files for docker-compose"
-    print "image-list     - Print list of images (bash format)"
+    print "gen             - Generate files for docker-compose"
+    print "image-list      - Print list of images (bash format)"
+    print "image-name node - Print image name for node"
+    print
+    print "Options:"
+    print "---------"
+    print "--image-registry name   - docker registry name to use in container name ([registry-name]/image-name)"
+    print "--image-postfix name    - postfix for use in container name (image-name[postfix])"
 
 
 if __name__ == "__main__":
@@ -132,7 +154,7 @@ if __name__ == "__main__":
         exit(0)
 
     env = Environment(
-        loader=FileSystemLoader('./templates')
+        loader=FileSystemLoader(['./templates', '/usr/share/daas/templates'])
     )
 
     confile = get_arg_param(['--confile', '-c'], '')
@@ -148,9 +170,16 @@ if __name__ == "__main__":
             print(ex)
             exit(1)
 
-    # print conf
     project = conf['project']
 
+    project['image-registry'] = get_arg_param(['--image-registry'], '')
+    project['image-postfix'] = get_arg_param(['--image-postfix'], '')
+    project['sorted_networks'] = sorted_networks(project)
+    project['extra_hosts'] = make_hosts(project)
+    project['nodes'] = make_nodes(project, 'controllers', project['image']['controller']) \
+                       + make_nodes(project, 'gui', project['image']['gui'])
+
+    # [command]: image-list
     if check_arg_param(['image-list']):
         s = ''
         for k, v in project['image'].items():
@@ -159,6 +188,21 @@ if __name__ == "__main__":
         print s.strip()
         exit(0)
 
+    # [command]: image-name
+    if check_arg_param(['image-name']):
+        nodename = get_arg_param(['image-name'])
+        if len(nodename) == 0:
+            print "(image-name): Unknown nodename. Use -h for help"
+            exit(1)
+        for n in project['nodes']:
+            if n['nodename'] == nodename:
+                print n['image-name']
+                exit(0)
+
+        print "(image-name): ERROR: Not found nodename '%s'" % nodename
+        exit(1)
+
+    # [command]: gen
     if not check_arg_param(['gen']):
         print "Unknown command. Use -h for help"
         exit(1)
@@ -168,11 +212,9 @@ if __name__ == "__main__":
     if not os.path.exists(outdir):
         os.mkdir(outdir)
 
-    project['sorted_networks'] = sorted_networks(project)
-
-    project['extra_hosts'] = make_hosts(project)
-    project['nodes'] = make_nodes(project, 'controllers', project['image']['controller']) \
-                       + make_nodes(project, 'gui', project['image']['gui'])
+    if get_arg_param(['gen']):
+        print "Unknown command. Use -h for help"
+        exit(1)
 
     # make docker-compose.yml
     dc_file = os.path.join(outdir, 'docker-compose.yml')
@@ -185,10 +227,11 @@ if __name__ == "__main__":
         if not os.path.exists(dirname):
             os.mkdir(dirname)
 
+        # make addons for gui (nginx, vnc config)
+        # ....
+
         # make Dockerfile
-        dockerfile = os.path.join(dirname, 'Dockerfile')
-        with open(dockerfile, 'w') as wfile:
-            wfile.write(env.get_template(n['Dockerfile.tpl']).render(project=project))
+        make_dockerfile(dirname, n['Dockerfile.tpl'], project)
 
         # copy addons
         addonsdir = 'addons'
@@ -197,3 +240,8 @@ if __name__ == "__main__":
                 src = os.path.join(addonsdir, f)
                 dest = os.path.join(dirname, f)
                 shutil.copy(src, dest)
+
+    # make Dockerfile for builder
+    dirname = os.path.join(outdir, 'builder')
+    tplname = 'Dockerfile.%s.tpl' % project['image']['builder']
+    make_dockerfile(dirname, tplname, project)
